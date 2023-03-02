f2aaae7c122be2e8d0e579e7e8eb23df9baaeca9
123-code <naranjojose256@gmail.com>
2023-03-02 17:19:56 -0500

    _-_

--- travel/src/Pages/Payments.jsx
+++ travel/src/Pages/Payments.jsx
@@ -1,7 +1,8 @@
 import React, { useRef, useState, useEffect } from 'react';
 import Web3Modal from 'web3modal';
 import { Web3Provider } from "@ethersproject/providers";
-import { ethers } from "ethers";
+import Web3 from 'web3';
+import { ethers,utils } from "ethers";
 import {PAYMENT_CONTRACT_ADDRESS,PAYMENT_CONTRACT_ABI} from '../Constants';
 import CoinbaseButton from '../Components/CoinbaseButton';
 import ConnectButton from '../Components/ConnectWalletButton';
@@ -10,73 +11,33 @@ import '../Styling/Payments.css';
 
 const Payments = () => {
     function MakePayment(){
-        const Web3ModalRef = useRef();
-        const [walletConnected, setWalletConnected] = useState(false);
-        const [address, setAddress] = useState("");
-        const [reserveAmount, setReserveAmount] = useState(0);
-    
-        const connectWallet = async () => {
-            try {
-              await getProviderOrSigner();
-              setWalletConnected(true);
-            } catch (err) {
-              console.error(err);
-            }
-          };
+   
 
-          const getReserveAmount = async()=>{
-            const provider = await getProviderOrSigner();
-            const contract = new ethers.Contract(PAYMENT_CONTRACT_ADDRESS,PAYMENT_CONTRACT_ABI,provider);
-            const amount = contract.amount();
-            setReserveAmount(amount);
-          }
+       async function Payment(){
 
-          const Checkout = async()=>{
-            try{
-                const signer = await getProviderOrSigner(true);
-                const contract = new ethers.Contract(PAYMENT_CONTRACT_ADDRESS,PAYMENT_CONTRACT_ABI,signer);
-                const tx = await contract.PayNow();
-                await tx.wait();
-            }catch(err){
-                console.error(err);
-            }
-          }
-    
-    
-        const getProviderOrSigner = async (needSigner = false) => {
-            const provider = await Web3ModalRef.current.connect();
-            const web3Provider = new Web3Provider(provider);
+        if (window.ethereum) {
+          window.alert("llego")
+          const provider = new Web3Provider(window.ethereum);
+          const signer = provider.getSigner();
+          const contract = new ethers.Contract(PAYMENT_CONTRACT_ADDRESS,PAYMENT_CONTRACT_ABI, signer);
+          console.log(contract.amount);
         
-            const { chainId } = await web3Provider.getNetwork();
-            if (chainId !== 5) {
-              window.alert("Change the network to goerli");
-              throw new Error("Change network to goerli");
-            }
+          contract.methods.PayNow().send({ value: 1200 })
+            .then((tx) => {
+              console.log(tx);
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+        } else {
+          window.alert(";")
+          console.error('MetaMask not detected');
+        }
+        }
         
-            if (needSigner) {
-              const signer = web3Provider.getSigner();
-              return signer;
-            }
-        
-            return web3Provider;
-          };
-          useEffect(() => {
-            if (!walletConnected) {
-              Web3ModalRef.current = new Web3Modal({
-                network: "mainnet",
-                cacheProvider: true,
-                providerOptions: {},
-                disableInjectedProvider: false,
-              });
-              connectWallet();
-    
-            }
-          }, []);
+       
 
-          useEffect(()=>{
-            getReserveAmount();
-          })
-    
+
         return(
             <div>
             <head>
@@ -91,11 +52,11 @@ const Payments = () => {
             <h3> you're paying us to the address: '0x390be0D2Da9eDC0F85Ff09bfBFC874Bc8Ab665A6' </h3>
             <form>
             <label>
-            <h3> Amount of ETH specified in reserve: {reserveAmount} </h3>
+            <h3> Amount of ETH specified in reserve:  </h3>
             <input type="text" name="name" className='entry' />
             </label>
             <div>
-            <button className="button" onClick = {Checkout}> Checkout </button>
+            <button className="button" onClick={ Payment()}> Checkout </button>
             </div>
             </form>
             </label>

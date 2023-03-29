029422dacf36d0dfb656985d17121c9e8a328dca
123-code <naranjojose256@gmail.com>
2023-03-29 14:11:50 -0500

    cambios  styling

--- travel/src/Components/Destinos.jsx
+++ travel/src/Components/Destinos.jsx
@@ -6,10 +6,10 @@ const Destinos = ({ imageSrc, text , title,button=false,link}) => {
   const navigation =  useNavigate();
   return (
     <div className="destinos-wrapper">
-      <h2 className='titleDestinos' style={{ flex: 1,fontFamily: '"Courier New", monospace' }}>{title}</h2>
+      <h2 className='titleDestinos' style={{ flex: 1,fontFamily: 'Times New Roman' }}>{title}</h2>
       <div style={{ display: 'flex', alignItems: 'flex-start' }}>
-        <img  className='imgDestinos' src={imageSrc} alt="Imagen del componente" style={{ marginRight: '20px', maxWidth: '50%', height: '250px', width: '250px' }} />
-        <p style={{ flex: 1, fontFamily: '"Courier New", monospace' }}>
+        <img  className='imgDestinos' src={imageSrc} alt="Imagen del componente" style={{ marginRight: '20px', maxWidth: '50%', height: '250px', width: '250px',fontFamily: 'Times New Roman' }} />
+        <p style={{ flex: 1, fontFamily: 'Times New Roman',fontSize:25 }}>
           {text}
         </p>
         {button ? <button className="destinos-button" onClick={()=>{navigation(link)}} style={{ backgroundColor: 'blue', color: 'white', padding: '10px', borderRadius: '5px', cursor: 'pointer' }}> Read More </button> : null}
--- travel/src/Pages/GalapagosPage.jsx
+++ travel/src/Pages/GalapagosPage.jsx
@@ -14,7 +14,14 @@ Las Islas Galápagos cuentan con una gran variedad de ecosistemas, desde playas 
 El archipiélago es un destino turístico popular por sus actividades de ecoturismo, que incluyen la observación de animales en su hábitat natural, el snorkel y el buceo. También se puede visitar el Centro de Interpretación en la isla de San Cristóbal, donde se puede aprender sobre la historia natural y la conservación de las islas.
 
 Sin embargo, la conservación de las Islas Galápagos es un desafío constante debido a la actividad humana y al turismo descontrolado. Por lo tanto, es importante visitar las islas con conciencia ambiental y seguir las regulaciones establecidas para mantener la riqueza natural del archipiélago para futuras generaciones." />
-        
+        <h2> Flora y fauna </h2>
+        <ul>
+            <p> Pinguino de nazca</p>
+            <p> Great hammerhead</p>
+            <p>Sea turtles</p>
+            <p>Flamingos</p>
+            <p> Iguana Marina </p>
+        </ul>
         <div className="sqr">
         <Square></Square>
     </div>

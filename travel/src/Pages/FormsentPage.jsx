7c24d20ec6cc005eac52efdd7895a68a8857e1c6
123-code <naranjojose256@gmail.com>
2023-04-27 09:54:44 -0500

    changes

---
+++ travel/src/Pages/FormsentPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ThankYouPage() {
+  return (
+    <div>
+      <h1>Thanks for your submission!</h1>
+      <p>Our reservations team will contact you in the next hours.</p>
+    </div>
+  );
+}
+
+export default ThankYouPage;

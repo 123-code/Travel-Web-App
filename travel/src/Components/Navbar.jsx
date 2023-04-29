7079ee26085a21b0d540b3713d090b56b9e02b49
123-code <naranjojose256@gmail.com>
2023-04-29 09:54:08 -0500

    cambios

--- travel/src/Components/Navbar.jsx
+++ travel/src/Components/Navbar.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import ConnectButton from "./ConnectWalletButton";
 import { useNavigate } from "react-router-dom";
-import '../Styling/Navbar.css'
-import Button from '@mui/material/Button';
-import photo from "./PHOTO-2023-04-27-14-08-53.jpg"
+import "../Styling/Navbar.css";
+import Button from "@mui/material/Button";
+import photo from "./PHOTO-2023-04-27-14-08-53.jpg";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
-const navigate = useNavigate();
+  const navigate = useNavigate();
   const [dropdown, setDropdown] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   const onMouseEnter = () => {
     setDropdown(true);
@@ -19,44 +21,95 @@ const navigate = useNavigate();
   };
 
   return (
-    <nav className="navbar" style={{  
-      display: 'flex', 
-      justifyContent: 'space-between', 
-      alignItems: 'center',
-      backgroundColor: 'white', 
-      color: 'black',
-      position: 'fixed', 
-      top: '0', 
-      width: '100%', 
-      height: 'auto',
-      zIndex: '1',
-      fontSize: '1.5vw', 
-      padding: '1vw' 
-    }}>
-      <div className="navbar__left" style={{ display: 'flex', alignItems: 'center' }}>
-        <h3 style={{ marginLeft: '20px' }}><img src={photo} style={{ width: "20%", height: "5%" }}/></h3>
-        <ul style={{ display: 'flex', listStyle: 'none', marginLeft: '20px' }}>
-          <li style={{ marginRight: '20px' }}>
-            <Link to="/" style={{ color: 'black' }}>Home</Link>
+    <nav
+      className="navbar"
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        backgroundColor: "white",
+        color: "black",
+        position: "fixed",
+        top: "0",
+        width: "100%",
+        height: "auto",
+        zIndex: "1",
+        fontSize: "1.5vw",
+        padding: "1vw",
+      }}
+    >
+      <div
+        className="navIcon"
+        onClick={() => setIsOpen(!isOpen)}
+        style={{ display: "flex", alignItems: "center" }}
+      >
+        <i className={isOpen ? "fas fa-times" : "fas fa-bars"}>
+          <GiHamburgerMenu />
+        </i>
+      </div>
+      <div>
+      <img
+            src={photo}
+            style={{ width: "10%", height: "10%" }}
+            alt="logo"
+          />
+      </div>
+      
+      <div
+        className="navbar__left"
+        style={{ display: isOpen ? "flex" : "none", alignItems: "center" }}
+      >
+        <h3 style={{ marginLeft: "20px" }}>
+         
+        </h3>
+        <ul
+          style={{ display: "flex", listStyle: "none", marginLeft: "20px" }}
+        >
+          <li style={{ marginRight: "20px" }}>
+            <Link to="/" style={{ color: "black" }}>
+              Home
+            </Link>
           </li>
-          <li style={{ marginRight: '20px' }}>
-            <Link to="/about" style={{ color: 'black' }}>About us </Link>
+          <li style={{ marginRight: "20px" }}>
+            <Link to="/about" style={{ color: "black" }}>
+              About us{" "}
+            </Link>
           </li>
-          <li style={{ marginRight: '20px' }} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
-          <Link to="/galapagos" style={{ color: 'black' }}>Galápagos</Link>
-          {dropdown && (
-            <ul className="dropdown">
-              <li><Link to="/high">Treasure</Link></li>
-              <li><Link to="/standard">Solaris</Link></li>
-              <li><Link to="/promotional">Archipel I</Link></li>
-              <li><Link to="/aqua">Aqua</Link></li>
-            </ul>
-          )}
-        </li>
-        <li style={{ marginRight: '20px' }} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
-          <Link to="/amazon" style={{ color: 'black' }}>Ecuador</Link>
-          
-        </li>
+          <li
+            style={{ marginRight: "20px" }}
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+          >
+            <Link to="/galapagos" style={{ color: "black" }}>
+              Galápagos
+            </Link>
+            {dropdown && (
+              <ul className="dropdown">
+                <li>
+                  <Link to="/high">Treasure</Link>
+                </li>
+                <li>
+                  <Link to="/standard">Solaris</Link>
+                </li>
+                <li>
+                  <Link to="/promotional">Archipel I</Link>
+                </li>
+                <li>
+                  <Link to="/aqua">Aqua</Link>
+                </li>
+              </ul>
+            )}
+          </li>
+          <li
+            style={{ marginRight: "20px" }}
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+          >
+            <Link to="/amazon" style={{ color: "black" }}>
+              Ecuador
+            </Link>
+          </li>
+         
      
           <li style={{ marginRight: '20px' }}>
       
@@ -79,6 +132,23 @@ const navigate = useNavigate();
         
       </ul>
       </div>
+      <style jsx>{`
+        @media (min-width: 768px) {
+          .navMenu {
+            display: flex !important;
+          }
+          .navIcon {
+            display: none !important;
+          }
+        }
+        @media (max-width: 768px) {
+          .navbar__left h3 {
+            margin: 0;
+            width: 50%;
+          }
+       
+        }
+      `}</style>
     </nav>
   );
 };
